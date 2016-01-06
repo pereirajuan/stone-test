39f84ff88a2243a29f672fd9e2cb6ed2471312f4
pereirajuan <hperezrjb@gmail.com>
2016-01-06 00:37:21 -0600

    [OGW] Added Seer's Lantern, Bearer of Silence, and Gift of Tusks.

--- Stone.Sets/src/mage/sets/oathofthegatewatch/GiftOfTusks.java
+++ Stone.Sets/src/mage/sets/oathofthegatewatch/GiftOfTusks.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,41 +20,46 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
+package mage.sets.oathofthegatewatch;
 
-package mage.game.permanent.token;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import mage.MageInt;
+import java.util.UUID;
+import mage.abilities.effects.Effect;
+import mage.abilities.effects.common.continuous.BecomesCreatureTargetEffect;
+import mage.cards.CardImpl;
 import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Rarity;
+import mage.game.permanent.token.ElephantToken;
+import mage.target.common.TargetCreaturePermanent;
 
 /**
  *
- * @author BetaSteward_at_googlemail.com
+ * @author fireshoes
  */
-public class ElephantToken extends Token {
+public class GiftOfTusks extends CardImpl {
 
-    final static private List<String> tokenImageSets = new ArrayList<>();
+    public GiftOfTusks(UUID ownerId) {
+        super(ownerId, 55, "Gift of Tusks", Rarity.UNCOMMON, new CardType[]{CardType.INSTANT}, "{U}");
+        this.expansionSetCode = "OGW";
 
-    static {
-        tokenImageSets.addAll(Arrays.asList("C14", "CNS", "DDD", "MM2", "WWK", "OGW"));
+        // Until end of turn, target creature loses all abilities and becomes a green Elephant with base power and toughness 3/3.
+        Effect effect = new BecomesCreatureTargetEffect(new ElephantToken(), true, false, Duration.EndOfTurn);
+        effect.setText("Until end of turn, target creature loses all abilities and becomes a green Elephant with base power and toughness 3/3");
+        this.getSpellAbility().addEffect(effect);
+        this.getSpellAbility().addTarget(new TargetCreaturePermanent());
     }
 
-    public ElephantToken() {
-        super("Elephant", "3/3 green Elephant creature token");
-        cardType.add(CardType.CREATURE);
-        color.setGreen(true);
-        subtype.add("Elephant");
-        power = new MageInt(3);
-        toughness = new MageInt(3);
-
-        availableImageSetCodes = tokenImageSets;
+    public GiftOfTusks(final GiftOfTusks card) {
+        super(card);
     }
 
+    @Override
+    public GiftOfTusks copy() {
+        return new GiftOfTusks(this);
+    }
 }
--- Stone.Sets/src/mage/sets/oathofthegatewatch/SeersLantern.java
+++ Stone.Sets/src/mage/sets/oathofthegatewatch/SeersLantern.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright 2010 BetaSteward_at_googlemail.com. All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without modification, are
  *  permitted provided that the following conditions are met:
- * 
+ *
  *     1. Redistributions of source code must retain the above copyright notice, this list of
  *        conditions and the following disclaimer.
- * 
+ *
  *     2. Redistributions in binary form must reproduce the above copyright notice, this list
  *        of conditions and the following disclaimer in the documentation and/or other materials
  *        provided with the distribution.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY BetaSteward_at_googlemail.com ``AS IS'' AND ANY EXPRESS OR IMPLIED
  *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BetaSteward_at_googlemail.com OR
@@ -20,41 +20,50 @@
  *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *  The views and conclusions contained in the software and documentation are those of the
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
+package mage.sets.oathofthegatewatch;
 
-package mage.game.permanent.token;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import mage.MageInt;
+import java.util.UUID;
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.costs.mana.GenericManaCost;
+import mage.abilities.effects.keyword.ScryEffect;
+import mage.abilities.mana.ColorlessManaAbility;
+import mage.cards.CardImpl;
 import mage.constants.CardType;
+import mage.constants.Rarity;
+import mage.constants.Zone;
 
 /**
  *
- * @author BetaSteward_at_googlemail.com
+ * @author fireshoes
  */
-public class ElephantToken extends Token {
+public class SeersLantern extends CardImpl {
 
-    final static private List<String> tokenImageSets = new ArrayList<>();
+    public SeersLantern(UUID ownerId) {
+        super(ownerId, 165, "Seer's Lantern", Rarity.COMMON, new CardType[]{CardType.ARTIFACT}, "{3}");
+        this.expansionSetCode = "OGW";
 
-    static {
-        tokenImageSets.addAll(Arrays.asList("C14", "CNS", "DDD", "MM2", "WWK", "OGW"));
+        // {T}: Add {C} to your mana pool.
+        this.addAbility(new ColorlessManaAbility());
+
+        // {2}, {T}: Scry 1.
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new ScryEffect(1), new GenericManaCost(2));
+        ability.addCost(new TapSourceCost());
+        this.addAbility(ability);
     }
 
-    public ElephantToken() {
-        super("Elephant", "3/3 green Elephant creature token");
-        cardType.add(CardType.CREATURE);
-        color.setGreen(true);
-        subtype.add("Elephant");
-        power = new MageInt(3);
-        toughness = new MageInt(3);
-
-        availableImageSetCodes = tokenImageSets;
+    public SeersLantern(final SeersLantern card) {
+        super(card);
     }
 
+    @Override
+    public SeersLantern copy() {
+        return new SeersLantern(this);
+    }
 }

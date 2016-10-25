447dce4377797138249394abd1abccb2c8e54fbe
pereirajuan <hperezrjb@gmail.com>
2016-10-25 15:55:46 -0500

    [C16] Added Primeval Protector, Crystalline Crawler, and Breya, Etherium Sculptor.

--- Stone.Sets/src/mage/cards/c/CrystallineCrawler.java
+++ Stone.Sets/src/mage/cards/c/CrystallineCrawler.java
@@ -25,48 +25,54 @@
  *  authors and should not be interpreted as representing official policies, either expressed
  *  or implied, of BetaSteward_at_googlemail.com.
  */
-package mage.cards.w;
+package mage.cards.c;
 
 import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.common.EntersBattlefieldAbility;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.RemoveCountersSourceCost;
+import mage.abilities.costs.common.TapSourceCost;
 import mage.abilities.dynamicvalue.common.ColorsOfManaSpentToCastCount;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
-import mage.abilities.keyword.TrampleAbility;
-import mage.abilities.keyword.VigilanceAbility;
+import mage.abilities.mana.AnyColorManaAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.Zone;
 import mage.counters.CounterType;
 
 /**
  *
- * @author LevelX2
+ * @author fireshoes
  */
-public class WoodlandWanderer extends CardImpl {
+public class CrystallineCrawler extends CardImpl {
 
-    public WoodlandWanderer(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId,setInfo,new CardType[]{CardType.CREATURE},"{3}{G}");
-        this.subtype.add("Elemental");
-        this.power = new MageInt(2);
-        this.toughness = new MageInt(2);
+    public CrystallineCrawler(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{4}");
 
-        // Vigilance
-        this.addAbility(VigilanceAbility.getInstance());
-        // Trample
-        this.addAbility(TrampleAbility.getInstance());
-        // <i>Converge</i> &mdash; Woodland Wanderer enters the battlefield with a +1/+1 counter on it for each color of mana spent to cast it.
+        this.subtype.add("Construct");
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        // <i>Converge</i> &mdash; Crystalline Crawler enters the battlefield with a +1/+1 counter on it for each color of mana spent to cast it.
         this.addAbility(new EntersBattlefieldAbility(
                 new AddCountersSourceEffect(CounterType.P1P1.createInstance(), ColorsOfManaSpentToCastCount.getInstance(), true),
-                null, "<i>Converge</i> &mdash; {this} enters the battlefield with a +1/+1 counter on it for each color of mana spent to cast it.", null));
+                null, "<i>Converge</i> - {this} enters the battlefield with a +1/+1 counter on it for each color of mana spent to cast it.", null));
+
+        // Remove a +1/+1 counter from Crystalline Crawler: Add one mana of any color to your mana pool.
+        this.addAbility(new AnyColorManaAbility(new RemoveCountersSourceCost(CounterType.P1P1.createInstance(1))));
+
+        // {T}: Put a +1/+1 counter on Crystalline Crawler.
+        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new AddCountersSourceEffect(CounterType.P1P1.createInstance(1)), new TapSourceCost()));
     }
 
-    public WoodlandWanderer(final WoodlandWanderer card) {
+    public CrystallineCrawler(final CrystallineCrawler card) {
         super(card);
     }
 
     @Override
-    public WoodlandWanderer copy() {
-        return new WoodlandWanderer(this);
+    public CrystallineCrawler copy() {
+        return new CrystallineCrawler(this);
     }
 }

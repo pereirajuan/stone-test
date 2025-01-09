pereirajuan/stone-test

commit 8bd147c8dffdfc0c795a2f7dbf62d3db50b21528

Create generic "Gains your choice of ability" effect, add [ACR] Assassin Initiate (#12619)
Create generic GainsChoiceOfAbilitiesEffect, covers both Target and Source cases.

Adds Assassin Initiate

Stone.Sets/src/mage/cards/u/UrzasAvenger.java
@@ -5,23 +5,17 @@ import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
-import mage.abilities.effects.OneShotEffect;
-import mage.abilities.effects.common.continuous.BoostSourceEffect;
-import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
+import mage.abilities.effects.common.GainsChoiceOfAbilitiesEffect;
+import mage.abilities.effects.common.continuous.BoostTargetEffect;
 import mage.abilities.keyword.BandingAbility;
 import mage.abilities.keyword.FirstStrikeAbility;
 import mage.abilities.keyword.FlyingAbility;
 import mage.abilities.keyword.TrampleAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.choices.Choice;
-import mage.choices.ChoiceImpl;
-import mage.constants.*;
-import mage.game.Game;
-import mage.players.Player;
+import mage.constants.CardType;
+import mage.constants.SubType;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -31,14 +25,19 @@ import java.util.UUID;
 public final class UrzasAvenger extends CardImpl {
 
     public UrzasAvenger(UUID ownerId, CardSetInfo setInfo) {
-        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT,CardType.CREATURE}, "{6}");
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT, CardType.CREATURE}, "{6}");
 
         this.subtype.add(SubType.SHAPESHIFTER);
         this.power = new MageInt(4);
         this.toughness = new MageInt(4);
 
         // {0}: Urza's Avenger gets -1/-1 and gains your choice of banding, flying, first strike, or trample until end of turn.
-        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD, new UrzasAvengerEffect(), new ManaCostsImpl<>("{0}")));
+        Ability ability = new SimpleActivatedAbility(new BoostTargetEffect(-1, -1)
+                .setText("{this} gets -1/-1"), new ManaCostsImpl<>("{0}"));
+        ability.addEffect(new GainsChoiceOfAbilitiesEffect(GainsChoiceOfAbilitiesEffect.TargetType.Source, "", true,
+                BandingAbility.getInstance(), FlyingAbility.getInstance(), FirstStrikeAbility.getInstance(), TrampleAbility.getInstance())
+                .concatBy("and"));
+        this.addAbility(ability);
     }
 
     private UrzasAvenger(final UrzasAvenger card) {
@@ -48,60 +47,5 @@ public final class UrzasAvenger extends CardImpl {
     @Override
     public UrzasAvenger copy() {
         return new UrzasAvenger(this);
-    }
-}
-
-class UrzasAvengerEffect extends OneShotEffect {
-
-    private static final Set<String> choices = new LinkedHashSet<>();
-    static {
-        choices.add("Banding");
-        choices.add("Flying");
-        choices.add("First strike");
-        choices.add("Trample");
-    }
-
-    public UrzasAvengerEffect() {
-        super(Outcome.AddAbility);
-        this.staticText = "{this} gets -1/-1 and gains your choice of banding, flying, first strike, or trample until end of turn";
-    }
-
-    private UrzasAvengerEffect(final UrzasAvengerEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public UrzasAvengerEffect copy() {
-        return new UrzasAvengerEffect(this);
-    }
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        if (controller != null) {
-            Choice choice = new ChoiceImpl(true);
-            choice.setMessage("Choose one");
-            choice.setChoices(choices);
-            if (controller.choose(outcome, choice, game)) {
-                Ability gainedAbility;
-                switch (choice.getChoice()) {
-                    case "Banding":
-                        gainedAbility = BandingAbility.getInstance();
-                        break;
-                    case "Flying":
-                        gainedAbility = FlyingAbility.getInstance();
-                        break;
-                    case "First strike":
-                        gainedAbility = FirstStrikeAbility.getInstance();
-                        break;
-                    default:
-                        gainedAbility = TrampleAbility.getInstance();
-                        break;
-                }
-                game.addEffect(new GainAbilitySourceEffect(gainedAbility, Duration.EndOfTurn), source);
-                game.addEffect(new BoostSourceEffect(-1, -1, Duration.EndOfTurn), source);
-                return true;
-            }
-        }
-        return false;
     }
 }

Stone.Sets/src/mage/cards/a/AetherChanneler.java
@@ -1,6 +1,5 @@
 package mage.cards.a;
 
-import java.util.UUID;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.Mode;
@@ -8,14 +7,16 @@ import mage.abilities.common.EntersBattlefieldTriggeredAbility;
 import mage.abilities.effects.common.CreateTokenEffect;
 import mage.abilities.effects.common.DrawCardSourceControllerEffect;
 import mage.abilities.effects.common.ReturnToHandTargetEffect;
-import mage.constants.SubType;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.SubType;
 import mage.filter.common.FilterNonlandPermanent;
 import mage.filter.predicate.mageobject.AnotherPredicate;
 import mage.game.permanent.token.BirdToken;
 import mage.target.common.TargetNonlandPermanent;
+
+import java.util.UUID;
 
 /**
  *
@@ -23,7 +24,7 @@ import mage.target.common.TargetNonlandPermanent;
  */
 public final class AetherChanneler extends CardImpl {
 
-    private static final FilterNonlandPermanent filter = new FilterNonlandPermanent("another nonland permanent");
+    private static final FilterNonlandPermanent filter = new FilterNonlandPermanent("another target nonland permanent");
 
     static {
         filter.add(AnotherPredicate.instance);
@@ -42,8 +43,7 @@ public final class AetherChanneler extends CardImpl {
         // * Return another target nonland permanent to its owner's hand.
         // * Draw a card.
         Ability ability = new EntersBattlefieldTriggeredAbility(new CreateTokenEffect(new BirdToken()));
-        Mode mode = new Mode(new ReturnToHandTargetEffect()
-                .setText("Return another target nonland permanent to its owner's hand."));
+        Mode mode = new Mode(new ReturnToHandTargetEffect());
         mode.addTarget(new TargetNonlandPermanent(filter));
         ability.addMode(mode);
         ability.addMode(new Mode(new DrawCardSourceControllerEffect(1)));

Stone.Sets/src/mage/cards/a/AlchemistsGift.java
@@ -1,18 +1,12 @@
 package mage.cards.a;
 
-import mage.abilities.Ability;
-import mage.abilities.effects.OneShotEffect;
+import mage.abilities.effects.common.GainsChoiceOfAbilitiesEffect;
 import mage.abilities.effects.common.continuous.BoostTargetEffect;
-import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
 import mage.abilities.keyword.DeathtouchAbility;
 import mage.abilities.keyword.LifelinkAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
-import mage.game.Game;
-import mage.players.Player;
 import mage.target.common.TargetCreaturePermanent;
 
 import java.util.UUID;
@@ -26,7 +20,9 @@ public final class AlchemistsGift extends CardImpl {
         super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{B}");
 
         // Target creature gets +1/+1 and gains your choice of deathtouch or lifelink until end of turn.
-        this.getSpellAbility().addEffect(new AlchemistsGiftEffect());
+        this.getSpellAbility().addEffect(new BoostTargetEffect(1, 1).setText("Target creature gets +1/+1"));
+        this.getSpellAbility().addEffect(new GainsChoiceOfAbilitiesEffect(GainsChoiceOfAbilitiesEffect.TargetType.Target, "", true,
+                DeathtouchAbility.getInstance(), LifelinkAbility.getInstance()).concatBy("and"));
         this.getSpellAbility().addTarget(new TargetCreaturePermanent());
     }
 
@@ -37,37 +33,5 @@ public final class AlchemistsGift extends CardImpl {
     @Override
     public AlchemistsGift copy() {
         return new AlchemistsGift(this);
-    }
-}
-
-class AlchemistsGiftEffect extends OneShotEffect {
-
-    AlchemistsGiftEffect() {
-        super(Outcome.Benefit);
-        staticText = "Target creature gets +1/+1 and gains your choice of deathtouch or lifelink until end of turn.";
-    }
-
-    private AlchemistsGiftEffect(final AlchemistsGiftEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public AlchemistsGiftEffect copy() {
-        return new AlchemistsGiftEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player player = game.getPlayer(source.getControllerId());
-        if (player == null) {
-            return false;
-        }
-        Ability ability = player.chooseUse(
-                outcome, "Deathtouch or lifelink?", null,
-                "Deathtouch", "Lifelink", source, game
-        ) ? DeathtouchAbility.getInstance() : LifelinkAbility.getInstance();
-        game.addEffect(new BoostTargetEffect(1, 1, Duration.EndOfTurn), source);
-        game.addEffect(new GainAbilityTargetEffect(ability, Duration.EndOfTurn), source);
-        return true;
     }
 }

Stone.Sets/src/mage/cards/a/ArgivianAvenger.java
@@ -4,28 +4,17 @@ import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.GenericManaCost;
-import mage.abilities.effects.OneShotEffect;
-import mage.abilities.effects.common.continuous.BoostSourceEffect;
-import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
+import mage.abilities.effects.common.GainsChoiceOfAbilitiesEffect;
+import mage.abilities.effects.common.continuous.BoostTargetEffect;
 import mage.abilities.keyword.DeathtouchAbility;
 import mage.abilities.keyword.FlyingAbility;
 import mage.abilities.keyword.HasteAbility;
 import mage.abilities.keyword.VigilanceAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.choices.Choice;
-import mage.choices.ChoiceImpl;
 import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
 import mage.constants.SubType;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -41,7 +30,12 @@ public final class ArgivianAvenger extends CardImpl {
         this.toughness = new MageInt(5);
 
         // {1}: Until end of turn, Argivian Avenger gets -1/-1 and gains your choice of flying, vigilance, deathtouch, or haste.
-        this.addAbility(new SimpleActivatedAbility(new ArgivianAvengerEffect(), new GenericManaCost(1)));
+        Ability ability = new SimpleActivatedAbility(new BoostTargetEffect(-1, -1)
+                .setText("Until end of turn, {this} gets -1/-1"), new GenericManaCost(1));
+        ability.addEffect(new GainsChoiceOfAbilitiesEffect(GainsChoiceOfAbilitiesEffect.TargetType.Source, "", false,
+                FlyingAbility.getInstance(), VigilanceAbility.getInstance(), DeathtouchAbility.getInstance(), HasteAbility.getInstance())
+                .concatBy("and"));
+        this.addAbility(ability);
     }
 
     private ArgivianAvenger(final ArgivianAvenger card) {
@@ -51,51 +45,5 @@ public final class ArgivianAvenger extends CardImpl {
     @Override
     public ArgivianAvenger copy() {
         return new ArgivianAvenger(this);
-    }
-}
-
-class ArgivianAvengerEffect extends OneShotEffect {
-
-    private static final Map<String, Ability> abilityMap = new HashMap<>();
-
-    static {
-        abilityMap.put("Flying", FlyingAbility.getInstance());
-        abilityMap.put("Vigilance", VigilanceAbility.getInstance());
-        abilityMap.put("Deathtouch", DeathtouchAbility.getInstance());
-        abilityMap.put("Haste", HasteAbility.getInstance());
-    }
-
-    ArgivianAvengerEffect() {
-        super(Outcome.Benefit);
-        staticText = "until end of turn, {this} gets -1/-1 " +
-                "and gains your choice of flying, vigilance, deathtouch, or haste";
-    }
-
-    private ArgivianAvengerEffect(final ArgivianAvengerEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public ArgivianAvengerEffect copy() {
-        return new ArgivianAvengerEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player player = game.getPlayer(source.getControllerId());
-        Permanent permanent = source.getSourcePermanentIfItStillExists(game);
-        if (player == null || permanent == null) {
-            return false;
-        }
-        game.addEffect(new BoostSourceEffect(-1, -1, Duration.EndOfTurn), source);
-        Choice choice = new ChoiceImpl(true);
-        choice.setMessage("Choose an ability");
-        choice.setChoices(new HashSet<>(abilityMap.keySet()));
-        player.choose(outcome, choice, game);
-        Ability ability = abilityMap.getOrDefault(choice.getChoice(), null);
-        if (ability != null) {
-            game.addEffect(new GainAbilitySourceEffect(ability, Duration.EndOfTurn), source);
-        }
-        return true;
     }
 }

Stone.Sets/src/mage/cards/a/AssassinInitiate.java
@@ -1,0 +1,44 @@
+package mage.cards.a;
+
+import mage.MageInt;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.mana.GenericManaCost;
+import mage.abilities.effects.common.GainsChoiceOfAbilitiesEffect;
+import mage.abilities.keyword.DeathtouchAbility;
+import mage.abilities.keyword.FlyingAbility;
+import mage.abilities.keyword.LifelinkAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+
+import java.util.UUID;
+
+/**
+ * @author notgreat
+ */
+public final class AssassinInitiate extends CardImpl {
+
+    public AssassinInitiate(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{B}");
+
+        this.subtype.add(SubType.HUMAN);
+        this.subtype.add(SubType.ASSASSIN);
+        this.power = new MageInt(1);
+        this.toughness = new MageInt(1);
+
+        // {1}: Assassin Initiate gains your choice of flying, deathtouch, or lifelink until end of turn.
+        this.addAbility(new SimpleActivatedAbility(new GainsChoiceOfAbilitiesEffect(GainsChoiceOfAbilitiesEffect.TargetType.Source,
+                FlyingAbility.getInstance(), DeathtouchAbility.getInstance(), LifelinkAbility.getInstance()),
+                new GenericManaCost(1)));
+    }
+
+    private AssassinInitiate(final AssassinInitiate card) {
+        super(card);
+    }
+
+    @Override
+    public AssassinInitiate copy() {
+        return new AssassinInitiate(this);
+    }
+}

Stone.Sets/src/mage/cards/a/AtraxasSkitterfang.java
@@ -1,14 +1,12 @@
 package mage.cards.a;
 
 import mage.MageInt;
-import mage.abilities.Ability;
 import mage.abilities.common.BeginningOfCombatTriggeredAbility;
 import mage.abilities.common.EntersBattlefieldAbility;
 import mage.abilities.common.delayed.ReflexiveTriggeredAbility;
 import mage.abilities.costs.common.RemoveCountersSourceCost;
-import mage.abilities.effects.OneShotEffect;
 import mage.abilities.effects.common.DoWhenCostPaid;
-import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
+import mage.abilities.effects.common.GainsChoiceOfAbilitiesEffect;
 import mage.abilities.effects.common.counter.AddCountersSourceEffect;
 import mage.abilities.keyword.DeathtouchAbility;
 import mage.abilities.keyword.FlyingAbility;
@@ -16,18 +14,12 @@ import mage.abilities.keyword.LifelinkAbility;
 import mage.abilities.keyword.VigilanceAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.choices.Choice;
-import mage.choices.ChoiceImpl;
-import mage.constants.*;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.TargetController;
 import mage.counters.CounterType;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
 import mage.target.common.TargetControlledCreaturePermanent;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -50,7 +42,9 @@ public final class AtraxasSkitterfang extends CardImpl {
         ));
 
         // At the beginning of combat on your turn, you may remove an oil counter from Atraxa's Skitterfang. When you do, target creature you control gains your choice of flying, vigilance, deathtouch, or lifelink until end of turn.
-        ReflexiveTriggeredAbility ability = new ReflexiveTriggeredAbility(new AtraxasSkitterfangEffect(), false);
+        ReflexiveTriggeredAbility ability = new ReflexiveTriggeredAbility(new GainsChoiceOfAbilitiesEffect(
+                FlyingAbility.getInstance(), VigilanceAbility.getInstance(), DeathtouchAbility.getInstance(), LifelinkAbility.getInstance())
+                .concatBy("and"), false);
         ability.addTarget(new TargetControlledCreaturePermanent());
         this.addAbility(new BeginningOfCombatTriggeredAbility(new DoWhenCostPaid(
                 ability,
@@ -66,50 +60,5 @@ public final class AtraxasSkitterfang extends CardImpl {
     @Override
     public AtraxasSkitterfang copy() {
         return new AtraxasSkitterfang(this);
-    }
-}
-
-class AtraxasSkitterfangEffect extends OneShotEffect {
-
-    private static final Map<String, Ability> abilityMap = new HashMap<>();
-
-    static {
-        abilityMap.put("Flying", FlyingAbility.getInstance());
-        abilityMap.put("Vigilance", VigilanceAbility.getInstance());
-        abilityMap.put("Deathtouch", DeathtouchAbility.getInstance());
-        abilityMap.put("Lifelink", LifelinkAbility.getInstance());
-    }
-
-    AtraxasSkitterfangEffect() {
-        super(Outcome.Benefit);
-        staticText = "target creature you control gains your choice " +
-                "of flying, vigilance, deathtouch, or lifelink until end of turn";
-    }
-
-    private AtraxasSkitterfangEffect(final AtraxasSkitterfangEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public AtraxasSkitterfangEffect copy() {
-        return new AtraxasSkitterfangEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player player = game.getPlayer(source.getControllerId());
-        Permanent permanent = source.getSourcePermanentIfItStillExists(game);
-        if (player == null || permanent == null) {
-            return false;
-        }
-        Choice choice = new ChoiceImpl(true);
-        choice.setMessage("Choose an ability");
-        choice.setChoices(new HashSet<>(abilityMap.keySet()));
-        player.choose(outcome, choice, game);
-        Ability ability = abilityMap.getOrDefault(choice.getChoice(), null);
-        if (ability != null) {
-            game.addEffect(new GainAbilityTargetEffect(ability, Duration.EndOfTurn), source);
-        }
-        return true;
     }
 }

Stone.Sets/src/mage/cards/b/BallroomBrawlers.java
@@ -3,26 +3,16 @@ package mage.cards.b;
 import mage.MageInt;
 import mage.abilities.Ability;
 import mage.abilities.common.AttacksTriggeredAbility;
-import mage.abilities.effects.OneShotEffect;
-import mage.abilities.effects.common.continuous.GainAbilityTargetEffect;
+import mage.abilities.effects.common.GainsChoiceOfAbilitiesEffect;
 import mage.abilities.keyword.FirstStrikeAbility;
 import mage.abilities.keyword.LifelinkAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
-import mage.constants.Duration;
-import mage.constants.Outcome;
 import mage.constants.SubType;
 import mage.filter.StaticFilters;
-import mage.game.Game;
-import mage.game.permanent.Permanent;
-import mage.players.Player;
 import mage.target.TargetPermanent;
-import mage.target.targetpointer.FixedTargets;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -39,8 +29,9 @@ public final class BallroomBrawlers extends CardImpl {
         this.toughness = new MageInt(5);
 
         // Whenever Ballroom Brawlers attacks, Ballroom Brawlers and up to one other target creature you control each gain your choice of first strike or lifelink until end of turn.
-        Ability ability = new AttacksTriggeredAbility(new BallroomBrawlersEffect()).withRuleTextReplacement(false);
-        ability.addTarget(new TargetPermanent(0, 1, StaticFilters.FILTER_CONTROLLED_ANOTHER_CREATURE));
+        Ability ability = new AttacksTriggeredAbility(new GainsChoiceOfAbilitiesEffect(GainsChoiceOfAbilitiesEffect.TargetType.Both,
+                FirstStrikeAbility.getInstance(), LifelinkAbility.getInstance())).withRuleTextReplacement(false);
+        ability.addTarget(new TargetPermanent(0, 1, StaticFilters.FILTER_ANOTHER_TARGET_CREATURE_YOU_CONTROL));
         this.addAbility(ability);
     }
 
@@ -51,45 +42,5 @@ public final class BallroomBrawlers extends CardImpl {
     @Override
     public BallroomBrawlers copy() {
         return new BallroomBrawlers(this);
-    }
-}
-
-class BallroomBrawlersEffect extends OneShotEffect {
-
-    BallroomBrawlersEffect() {
-        super(Outcome.Benefit);
-        staticText = "{this} and up to one other target creature you control " +
-                "both gain your choice of first strike or lifelink until end of turn";
-    }
-
-    private BallroomBrawlersEffect(final BallroomBrawlersEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public BallroomBrawlersEffect copy() {
-        return new BallroomBrawlersEffect(this);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player player = game.getPlayer(source.getControllerId());
-        if (player == null) {
-            return false;
-        }
-        List<Permanent> permanents = new ArrayList<>();
-        permanents.add(source.getSourcePermanentIfItStillExists(game));
-        permanents.add(game.getPermanent(getTargetPointer().getFirst(game, source)));
-        permanents.removeIf(Objects::isNull);
-        if (permanents.isEmpty()) {
-            return false;
-        }
-        Ability ability = player.chooseUse(
-                outcome, "Choose first strike or lifelink", null,
-                "First Strike", "Lifelink", source, game
-        ) ? FirstStrikeAbility.getInstance() : LifelinkAbility.getInstance();
-        game.addEffect(new GainAbilityTargetEffect(ability, Duration.EndOfTurn)
-                .setTargetPointer(new FixedTargets(permanents, game)), source);
-        return true;
     }
 }

Stone.Sets/src/mage/cards/b/ButcherOfTheHorde.java
@@ -1,28 +1,20 @@
 package mage.cards.b;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
 import mage.MageInt;
-import mage.MageObject;
-import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.common.SacrificeTargetCost;
-import mage.abilities.effects.ContinuousEffect;
-import mage.abilities.effects.OneShotEffect;
-import mage.abilities.effects.common.continuous.GainAbilitySourceEffect;
+import mage.abilities.effects.common.GainsChoiceOfAbilitiesEffect;
 import mage.abilities.keyword.FlyingAbility;
 import mage.abilities.keyword.HasteAbility;
 import mage.abilities.keyword.LifelinkAbility;
 import mage.abilities.keyword.VigilanceAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.choices.Choice;
-import mage.choices.ChoiceImpl;
-import mage.constants.*;
+import mage.constants.CardType;
+import mage.constants.SubType;
 import mage.filter.StaticFilters;
-import mage.game.Game;
-import mage.players.Player;
+
+import java.util.UUID;
 
 /**
  *
@@ -39,9 +31,11 @@ public final class ButcherOfTheHorde extends CardImpl {
 
         // Flying
         this.addAbility(FlyingAbility.getInstance());
+
         // Sacrifice another creature: Butcher of the Horde gains your choice of vigilance, lifelink, or haste until end of turn.
-        this.addAbility(new SimpleActivatedAbility(Zone.BATTLEFIELD,
-                new ButcherOfTheHordeEffect(), new SacrificeTargetCost(StaticFilters.FILTER_CONTROLLED_ANOTHER_CREATURE)));
+        this.addAbility(new SimpleActivatedAbility(new GainsChoiceOfAbilitiesEffect(GainsChoiceOfAbilitiesEffect.TargetType.Source,
+                VigilanceAbility.getInstance(), LifelinkAbility.getInstance(), HasteAbility.getInstance()),
+                new SacrificeTargetCost(StaticFilters.FILTER_CONTROLLED_ANOTHER_CREATURE)));
     }
 
     private ButcherOfTheHorde(final ButcherOfTheHorde card) {
@@ -52,59 +46,4 @@ public final class ButcherOfTheHorde extends CardImpl {
     public ButcherOfTheHorde copy() {
         return new ButcherOfTheHorde(this);
     }
-}
-
-class ButcherOfTheHordeEffect extends OneShotEffect {
-
-    ButcherOfTheHordeEffect() {
-        super(Outcome.AddAbility);
-        staticText = "{this} gains your choice of vigilance, lifelink, or haste until end of turn";
-    }
-
-    private ButcherOfTheHordeEffect(final ButcherOfTheHordeEffect effect) {
-        super(effect);
-    }
-
-    @Override
-    public boolean apply(Game game, Ability source) {
-        Player controller = game.getPlayer(source.getControllerId());
-        MageObject sourceObject = game.getObject(source);
-        if (sourceObject != null && controller != null) {
-            Choice abilityChoice = new ChoiceImpl(true);
-            abilityChoice.setMessage("Choose an ability to add");
-
-            Set<String> abilities = new HashSet<>();
-            abilities.add(VigilanceAbility.getInstance().getRule());
-            abilities.add(LifelinkAbility.getInstance().getRule());
-            abilities.add(HasteAbility.getInstance().getRule());
-            abilityChoice.setChoices(abilities);
-            controller.choose(Outcome.AddAbility, abilityChoice, game);
-            if (!abilityChoice.isChosen()) {
-                return false;
-            }
-            String chosen = abilityChoice.getChoice();
-            Ability ability = null;
-            if (VigilanceAbility.getInstance().getRule().equals(chosen)) {
-                ability = VigilanceAbility.getInstance();
-            } else if (LifelinkAbility.getInstance().getRule().equals(chosen)) {
-                ability = LifelinkAbility.getInstance();
-            } else if (HasteAbility.getInstance().getRule().equals(chosen)) {
-                ability = HasteAbility.getInstance();
-            }
-
-            if (ability != null) {
-                game.informPlayers(sourceObject.getLogName() + ": " + controller.getLogName() + " has chosen: " + chosen);
-                ContinuousEffect effect = new GainAbilitySourceEffect(ability, Duration.EndOfTurn);
-                game.addEffect(effect, source);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public ButcherOfTheHordeEffect copy() {
-        return new ButcherOfTheHordeEffect(this);
-    }
-
 }

commit 425bcb37252470d943fadcf31b83f97e7978b8be

fix #13215 (Urza's Avenger, regression from #12619)

Stone.Sets/src/mage/cards/u/UrzasAvenger.java
@@ -1,4 +1,3 @@
-
 package mage.cards.u;
 
 import mage.MageInt;
@@ -6,7 +5,7 @@ import mage.abilities.Ability;
 import mage.abilities.common.SimpleActivatedAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.abilities.effects.common.GainsChoiceOfAbilitiesEffect;
-import mage.abilities.effects.common.continuous.BoostTargetEffect;
+import mage.abilities.effects.common.continuous.BoostSourceEffect;
 import mage.abilities.keyword.BandingAbility;
 import mage.abilities.keyword.FirstStrikeAbility;
 import mage.abilities.keyword.FlyingAbility;
@@ -14,12 +13,12 @@ import mage.abilities.keyword.TrampleAbility;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
 import mage.constants.CardType;
+import mage.constants.Duration;
 import mage.constants.SubType;
 
 import java.util.UUID;
 
 /**
- *
  * @author Styxo & L_J
  */
 public final class UrzasAvenger extends CardImpl {
@@ -32,7 +31,7 @@ public final class UrzasAvenger extends CardImpl {
         this.toughness = new MageInt(4);
 
         // {0}: Urza's Avenger gets -1/-1 and gains your choice of banding, flying, first strike, or trample until end of turn.
-        Ability ability = new SimpleActivatedAbility(new BoostTargetEffect(-1, -1)
+        Ability ability = new SimpleActivatedAbility(new BoostSourceEffect(-1, -1, Duration.EndOfTurn)
                 .setText("{this} gets -1/-1"), new ManaCostsImpl<>("{0}"));
         ability.addEffect(new GainsChoiceOfAbilitiesEffect(GainsChoiceOfAbilitiesEffect.TargetType.Source, "", true,
                 BandingAbility.getInstance(), FlyingAbility.getInstance(), FirstStrikeAbility.getInstance(), TrampleAbility.getInstance())